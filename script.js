7eac9cc5a8594567b452913d454aaad71e2425be
Et-Shafa <naadiramalaika.atsiila@gmail.com>
2023-07-02 03:34:16 +0700

    resolve number 6

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// let name = document.getElementById("name");
-// let name = document.getElementById("name");
-// let name = document.getElementById("name");
-// let name = document.getElementById("name");
-// let name = document.getElementById("name");
+function handleGetFormData(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let city = document.getElementById("city").value;
+    let zipCode = document.getElementById("zip-code").value;
+    let status = document.getElementById("status").checked;
 
-// function handleGetFormData
+    return {
+        name : name, 
+        email : email, 
+        city : city, 
+        zipCode : zipCode, 
+        status : status
+        };
+}
+
+// function isNumber(string){
+//     if(isNaN(string)){
+//         return false;
+//     }else{
+//         return true;
+//     }
+// }
